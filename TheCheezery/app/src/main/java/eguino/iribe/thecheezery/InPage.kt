1194961b31cabd02344b314e554489ffab5a2b1a
Israel Eguino <israel.eguino204244@potros.itson.edu.mx>
2021-02-21 13:47:19 -0700

    Initialize

---
+++ TheCheezery/app/src/main/java/eguino/iribe/thecheezery/InPage.kt
@@ -0,0 +1,11 @@
+package eguino.iribe.thecheezery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_in_page)
+    }
+}
---
+++ TheCheezery/app/src/main/java/eguino/iribe/thecheezery/registerpage.kt
@@ -0,0 +1,29 @@
+package eguino.iribe.thecheezery
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.RadioButton
+import android.widget.TextView
+
+class registerpage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registerpage)
+
+        val button: Button = findViewById(R.id.button)
+        val accept: CheckBox = findViewById(R.id.accept)
+        val terms: TextView = findViewById(R.id.terms)
+
+        button.setOnClickListener(){
+            if(accept.isChecked) {
+                val intent = Intent(this, InPage::class.java)
+                startActivity(intent)
+            } else {
+                terms.setVisibility(TextView.VISIBLE);
+            }
+        }
+    }
+}
